aa0a88955d2483f501c06bedfe3b0fc29b68c08c
runegranath <rune.granath@gmail.com>
2026-02-18 05:49:25 +0100

    fixad sökväg

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-
+base: '/Uppgift-2-Frontendbaserad-webbutveckling',
   build: {
     rollupOptions: {
       input: {
