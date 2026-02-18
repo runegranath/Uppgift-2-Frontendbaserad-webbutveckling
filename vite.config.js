8393fe23136f4aae0e411e059e00f989373c571b
runegranath <rune.granath@gmail.com>
2026-02-18 06:00:35 +0100

    ytterligare felsökning i vites config för publicering

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+
 
 export default defineConfig({
 base: '/Uppgift-2-Frontendbaserad-webbutveckling',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "./index.html"),
-      },
-    },
-  },
+  
 });
